0cf9b25620332ce2cc71f3df9c13f4b0dac63f57
Nicholas Berlette <nick@berlette.com>
2021-07-13 10:35:46 -0700

    Deploy Next.js to gh-pages

---
+++ out/_next/static/GLq3fNzDR9XNn7EjS1myS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/244-fb2dc9c4262a53f43079.js","static/chunks/pages/index-515536c6dcf1def34e0a.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
