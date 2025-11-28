92390675f7409befbcdfcf986922035bb5506a52
Nicholas Berlette <nick@berlette.com>
2025-11-27 19:06:41 -0800

    chore: remove old config files

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production'
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-
-module.exports = withNextra({
-  assetPrefix: !debug ? '/' : '',
-})
