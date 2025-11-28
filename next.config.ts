d31804ec4910d5ed32b7e7d8ca915d6d0604047f
Nicholas Berlette <nick@berlette.com>
2025-11-27 18:50:52 -0800

    refactor(config): update next config, switch to .ts

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import nextra from "nextra";
+import process from "node:process";
+import nextraConfig from "./theme.config.ts";
+
+const env = process.env || {} as never;
+const theme = env.NEXT_PUBLIC_NEXTRA_THEME ||= "nextra-theme-docs";
+const debug = env.NODE_ENV !== "production";
+const assetPrefix = env.NEXT_PUBLIC_ASSET_PREFIX || (debug ? "" : "/");
+
+const withNextra = nextra(nextraConfig);
+
+export default withNextra({
+  assetPrefix,
+  compiler: {},
+  compress: true,
+  experimental: {},
+});
